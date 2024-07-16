fea9663f8e4c145ef5ebb583ee93bcabf051a652
Pranav More <prnvmore9@gmail.com>
2024-07-16 19:09:50 +0530

    added users table creation code

--- 1.0-JDBCTransaction/src/main/java/more/pranav/SimpleJdbcTransaction.java
+++ 1.0-JDBCTransaction/src/main/java/more/pranav/SimpleJdbcTransaction.java
@@ -26,14 +26,36 @@ public class SimpleJdbcTransaction {
     }
 
     private void createUsersTable() {
+        try {
+            Connection conn = DriverManager.getConnection(Constants.DB_URL, Constants.USER, Constants.PASS);
+            String CREATE_QUERY = "CREATE TABLE IF NOT EXISTS public.user_master" +
+                    "(id integer NOT NULL," +
+                    "    username character varying(100) NOT NULL," +
+                    "    first_name character varying(150) NOT NULL," +
+                    "    last_name character varying(150) NOT NULL," +
+                    "    email_id character varying(250) NOT NULL," +
+                    "    status character varying(50) NOT NULL," +
+                    "    created_by character varying(100)," +
+                    "    created_at date," +
+                    "    CONSTRAINT user_master_pkey PRIMARY KEY (id))";
+            Statement stmt = conn.createStatement();
+            stmt.execute(CREATE_QUERY);
 
+            System.out.println("Successfully created users table");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     private void insertUser(User user) {
 
+        // Insert your code here to add new user in database.
+
     }
 
     private void displayUsers() {
+
+        // Insert your code here to fetch all users from database.
 
     }
 }
