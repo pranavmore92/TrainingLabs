0e1e194764e997185f00eb2a9244b96abd4f7dfb
Pranav More <prnvmore9@gmail.com>
2024-08-07 18:10:43 +0530

    updated code for transaction management

--- 2.0-SpringTransaction/src/main/java/more/pranav/springtransaction/repository/BusRepository.java
+++ 2.0-SpringTransaction/src/main/java/more/pranav/springtransaction/repository/BusRepository.java
@@ -3,7 +3,12 @@ package more.pranav.springtransaction.repository;
 import more.pranav.springtransaction.entity.Bus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
-public interface BusRepository extends JpaRepository<Bus, Long> {
+public interface BusRepository extends JpaRepository<Bus, Integer> {
+
 }
