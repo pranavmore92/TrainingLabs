edfcfcc8d01d82528223033dc6ce9e2c104be717
Pranav More <prnvmore9@gmail.com>
2024-08-07 18:11:23 +0530

    updated code for transaction management

---
+++ 2.0-SpringTransaction/src/main/java/more/pranav/springtransaction/service/AuditService.java
@@ -0,0 +1,25 @@
+package more.pranav.springtransaction.service;
+
+import more.pranav.springtransaction.entity.Audit;
+import more.pranav.springtransaction.entity.Booking;
+import more.pranav.springtransaction.entity.Payment;
+import more.pranav.springtransaction.repository.AuditRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class AuditService {
+
+    @Autowired
+    private AuditRepository auditRepository;
+    private int auditCounter = 1;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void logEvent(Booking booking, Payment payment){
+        Audit audit = Audit.build(auditCounter, "BOOKING", booking.getId(), payment.getId(), booking.getStatus(), booking.getCreatedBy(), booking.getCreatedAt());
+        auditRepository.save(audit);
+        auditCounter++;
+    }
+}
